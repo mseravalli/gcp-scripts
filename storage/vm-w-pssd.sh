f765b4481ee06ae82362fc99c11c3482d7986109
Marco Seravalli <seravalli@google.com>
2018-02-15 16:40:32 +0100

    setup machines using timestamp as part of the name

--- storage/vm-w-pssd.sh
+++ storage/vm-w-pssd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Optional login if you are not already logged in
-# gcloud  auth login
-# gcloud config set project seravalli-test-190910
+gcloud auth login
+gcloud config set seravalli-test
 
 set -x
 
@@ -12,7 +12,7 @@ ZONE="${REGION}-b"
 gcloud compute disks create google-local-ssd-0 \
   --zone=${ZONE} --type=pd-ssd --size=500GB
 
-gcloud compute instances create instance-1 \
+gcloud compute instances create "instance-%(date '+%s')" \
   --zone ${ZONE} --machine-type "n1-standard-16" \
   --disk "name=google-local-ssd-0,device-name=google-local-ssd-0,mode=rw,boot=no" 
 
