660fc0c342e99fd5660303852975c81b12dc4fc7
Marco Seravalli <seravalli@google.com>
2018-02-16 19:21:49 +0100

    format and mount a disk on sles 12

--- compute/dm/install-echo.sh
+++ compute/dm/install-echo.sh
@@ -3,11 +3,11 @@
 # sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard \
 #  /dev/disk/by-id/google-persistent-disk
 
-yes | sudo /sbin/mkfs -t ext3 /dev/disk/by-id/google-persistent-disk
+yes | sudo mkfs.ext3 /dev/disk/by-id/google-persistent-disk
 
 sudo mkdir -p /mnt/disks/google-persistent-disk
 
 sudo mount /dev/disk/by-id/google-persistent-disk \
   /mnt/disks/google-persistent-disk
 
-sudo chmod 666 -R /mnt/disks/
+sudo chmod 777 -R /mnt/disks/google-persistent-disk
