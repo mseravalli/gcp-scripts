00ee84676884850c286b1612f1195e88af4be4f6
Marco Seravalli <seravalli@google.com>
2018-03-12 15:28:26 +0100

    allow for easier swapping between ssd and hdd

--- storage/vm-w-pssd.sh
+++ storage/vm-w-pssd.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Optional login if you are not already logged in
 gcloud auth login
-gcloud config set seravalli-test
+gcloud config set project test-seravalli
 
-set -x
+REGION="europe-west3"
+ZONE="${REGION}-a"
 
-REGION="us-east1"
-ZONE="${REGION}-b"
+# DISK_TYPE=pd-ssd
+DISK_TYPE=pd-standard
 
-gcloud compute disks create google-local-ssd-0 \
-  --zone=${ZONE} --type=pd-ssd --size=500GB
+DISK_NAME="google-${DISK_TYPE}-$(date '+%s')"
+gcloud compute disks create ${DISK_NAME} \
+  --zone=${ZONE} --type=${DISK_TYPE} --size=500GB
 
-gcloud compute instances create "instance-%(date '+%s')" \
-  --zone ${ZONE} --machine-type "n1-standard-16" \
-  --disk "name=google-local-ssd-0,device-name=google-local-ssd-0,mode=rw,boot=no" 
+gcloud compute instances create "instance-$(date '+%s')" \
+  --zone ${ZONE} --machine-type "n1-standard-1" \
+  --disk "name=${DISK_NAME},device-name=${DISK_NAME},mode=rw,boot=no" 
 
