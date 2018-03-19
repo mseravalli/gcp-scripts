88ddd83e6f485f65faa0b79ed83d21a819e344a1
Marco Seravalli <seravalli@google.com>
2018-03-19 14:21:05 +0100

    create networks with dm

---
+++ networking/dm/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+# apt-get update && apt-get upgrade -y
+#
+# apt-get install -y vim less bzip2
+# echo "alias ll=\"ls -lah\"" >> /home/seravalli/.bashrc
+# echo "set -o vi" >> /home/seravalli/.bashrc
+#
+# echo "alias ll=\"ls -lah\"" >> /root/.bashrc
+# echo "set -o vi" >> /root/.bashrc
+#
+# update-alternatives --set editor /usr/bin/vim.basic
