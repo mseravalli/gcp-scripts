31df7347cdc48d6b7c306afcbe381e090b4d3fa5
Marco Seravalli <seravalli@google.com>
2018-04-24 15:03:30 +0200

    remove project from command line

--- compute/serialoutput.sh
+++ compute/serialoutput.sh
@@ -4,6 +4,6 @@ echo 'Specify --start=1' > lastline
 
 while true
 do 
-  cat lastline | awk '{print $2}' | xargs -I {} bash -c "gcloud compute --project=test-seravalli instances get-serial-port-output $1 --zone=europe-west3-a {} 2> lastline"
+  cat lastline | awk '{print $2}' | xargs -I {} bash -c "gcloud compute instances get-serial-port-output $1 --zone=europe-west1-c {} 2> lastline"
   sleep .5
 done
