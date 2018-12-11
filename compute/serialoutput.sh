970887dff2a194b3f23a341415e7d5e08e995935
Marco Seravalli <seravalli@google.com>
2018-12-11 11:31:53 +0100

    if there is not new information from the serial console don't print out anything

--- compute/serialoutput.sh
+++ compute/serialoutput.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 LASTLINE=$(mktemp)
+OUTPUT=$(mktemp)
 
 echo 'Specify --start=1' > $LASTLINE
 
 while true
 do 
-  cat $LASTLINE | awk '{print $2}' | xargs -I {} bash -c "gcloud compute instances get-serial-port-output $1 --zone=europe-west4-c {} 2> $LASTLINE"
+  cat $LASTLINE | awk '{print $2}' | xargs -I {} bash -c "gcloud compute instances get-serial-port-output $1 --zone=europe-west4-c {} 2> $LASTLINE 1> $OUTPUT"
+
+  if [[ $(wc $OUTPUT | awk '{print $1}') -gt 1 ]]; then
+    cat $OUTPUT 
+  fi
+
   sleep .5
 done
